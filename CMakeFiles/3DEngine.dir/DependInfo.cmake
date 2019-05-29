ec7d3654c2c2c2835eb4ecd08bb92bdc2651bb90
Agah Ebrahimi <agahebr@gmail.com>
2019-05-29 13:51:53 -0400

    Re-Init

---
+++ CMakeFiles/3DEngine.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/agah/Desktop/Engine/src/main.cpp" "/home/agah/Desktop/Engine/CMakeFiles/3DEngine.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "packages/glfw-3.1.2/include"
+  "packages/glm-0.9.7.1"
+  "packages/glew-1.13.0/include"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/agah/Desktop/Engine/packages/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/agah/Desktop/Engine/packages/CMakeFiles/GLEW_1130.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/3DEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3DEngine.dir/src/main.cpp.o"
+  "build/3DEngine.pdb"
+  "build/3DEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
